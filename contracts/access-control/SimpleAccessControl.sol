20cef6ec3221adbafa2e3a18bfc34376f47ce3de
tayutaedomo <tayutaedomo@gmail.com>
2023-09-18 16:40:42 +0900

    Add hasRole check

--- contracts/access-control/SimpleAccessControl.sol
+++ contracts/access-control/SimpleAccessControl.sol
@@ -4,9 +4,20 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract SimpleAccessControl is AccessControl {
-    constructor() {}
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant HELLO_ROLE = keccak256("HELLO_ROLE");
 
-    function hello() public pure returns (string memory) {
+    constructor() {
+        _grantRole(ADMIN_ROLE, msg.sender);
+        _grantRole(HELLO_ROLE, msg.sender);
+        _setRoleAdmin(HELLO_ROLE, ADMIN_ROLE);
+    }
+
+    function hello() public view returns (string memory) {
+        require(
+            hasRole(HELLO_ROLE, msg.sender),
+            "Caller does'n have a HELLO_ROLE"
+        );
         return "hello";
     }
 }
--- test/SimpleAccessControl.ts
+++ test/SimpleAccessControl.ts
@@ -14,11 +14,39 @@ describe("SimpleAccessControl", function () {
     return { owner, account1, accessControl };
   }
 
+  describe("Deployment", function () {
+    it("should set the right owner", async function () {
+      const { accessControl, owner } = await loadFixture(deployContract);
+      expect(
+        await accessControl.hasRole(
+          await accessControl.ADMIN_ROLE(),
+          owner.address,
+        ),
+      ).to.equal(true);
+    });
+
+    it("Owner should have HELLO_ROLE", async function () {
+      const { accessControl, owner } = await loadFixture(deployContract);
+      expect(
+        await accessControl.hasRole(
+          await accessControl.HELLO_ROLE(),
+          owner.address,
+        ),
+      ).to.equal(true);
+    });
+  });
+
   describe("hello", function () {
     it("should return correct value", async function () {
-      const { accessControl } = await loadFixture(deployContract);
+      const { accessControl, owner } = await loadFixture(deployContract);
+      expect(await accessControl.connect(owner).hello()).to.equal("hello");
+    });
 
-      expect(await accessControl.hello()).to.equal("hello");
+    it("should revert if not role granted", async function () {
+      const { accessControl, account1 } = await loadFixture(deployContract);
+      await expect(accessControl.connect(account1).hello()).to.be.revertedWith(
+        "Caller does'n have a HELLO_ROLE",
+      );
     });
   });
 });
